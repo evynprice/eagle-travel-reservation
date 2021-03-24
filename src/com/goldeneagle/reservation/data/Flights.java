ef1d9908679c75440426ce4715a41f915d899a9c
evynprice <me@evynprice.com>
2021-03-24 16:44:37 -0400

    Added Flights class

---
+++ src/com/goldeneagle/reservation/data/Flights.java
@@ -0,0 +1,21 @@
+package com.goldeneagle.reservation.data;
+
+import com.goldeneagle.reservation.resources.Flight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Flights {
+    private List<Flight> flights = new ArrayList<>();
+
+    public Flights() {
+    }
+
+    public List<Flight> getFlights() {
+        return this.flights;
+    }
+
+    public void addFlight(Flight flight) {
+        this.flights.add(flight);
+    }
+}
