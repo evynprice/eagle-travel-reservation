6d849653c42618f875540b5dc94221178daaddca
evynprice <me@evynprice.com>
2021-03-24 16:09:48 -0400

    Add necessary Flight class methods

--- src/com/goldeneagle/reservation/resources/Flight.java
+++ src/com/goldeneagle/reservation/resources/Flight.java
@@ -1,21 +1,56 @@
 package com.goldeneagle.reservation.resources;
 
-import java.util.Date;
+import java.time.Duration;
+import java.time.LocalDate;
 
 public class Flight {
     private final String name;
-    private final Date date;
+    private final LocalDate date;
+    private final Duration duration;
     private final City departureCity;
     private final City arrivalCity;
     private final int totalSeats;
     private int availableSeats;
 
-    public Flight(String name, Date date, City departureCity, City arrivalCity, int totalSeats) {
+    public Flight(String name, LocalDate date, Duration duration, City departureCity, City arrivalCity, int totalSeats) {
         this.name = name;
         this.date = date;
+        this.duration = duration;
         this.departureCity = departureCity;
         this.arrivalCity = arrivalCity;
         this.totalSeats = totalSeats;
         this.availableSeats = totalSeats;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public LocalDate getDate() {
+        return this.date;
+    }
+
+    public Duration getDuration() {
+        return this.duration;
+    }
+
+    public City getDepartureCity() {
+        return this.departureCity;
+    }
+
+    public City getArrivalCity() {
+        return this.arrivalCity;
+    }
+
+    public int getTotalSeats() {
+        return this.totalSeats;
+    }
+
+    public int getAvailableSeats() {
+        return this.availableSeats;
+    }
+
+    public void takeSeat() {
+        this.availableSeats -= 1;
     }
 }
