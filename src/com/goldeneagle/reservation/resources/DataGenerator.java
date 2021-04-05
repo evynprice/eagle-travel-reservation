46e640b5b5b99bd6d28426b20fd844c0a0a01921
evynprice <me@evynprice.com>
2021-04-05 17:17:04 -0400

    Cleaned up documentation and unused methods

--- src/com/goldeneagle/reservation/resources/DataGenerator.java
+++ src/com/goldeneagle/reservation/resources/DataGenerator.java
@@ -10,100 +10,128 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+
 /**
- *
- * @author Joeseph Jackson
+ * This class will be used to generate the random data used by the program. While in the real world the list of flights,
+ * cities, and seats would be stored in a database, this project chooses to randomly select that data from a list of
+ * possible options to maintain simplicity and focus on understanding.
  */
 public class DataGenerator {
-    private List<City> cities = new ArrayList<>();
-    private List<Flight> flights = new ArrayList<>();
-    private Random random = new Random();
+    // initialize instance variables
+    private final List<City> cities = new ArrayList<>();
+    private final List<Flight> flights = new ArrayList<>();
+    private final Random random = new Random();
 
+    /**
+     * The default constructor will call on private object methods in order to generate the data required
+     */
     public DataGenerator() {
+        this.generateCities();
+        this.generateFlights(6); // generate six flights
+    }
 
-        // generate cities
+    /**
+     * This method adds all of the possible cities for departures and arrivals to the cities object
+     */
+    private void generateCities() {
         this.cities.add(new City("Chattanooga, TN"));
         this.cities.add(new City("Nashville, TN"));
         this.cities.add(new City("Chicago, IL"));
         this.cities.add(new City("Los Angeles, CA"));
         this.cities.add(new City("Atlanta, GA"));
-
-        this.generateFlights();
     }
 
-    private void generateFlights() {
-        // newFlight(String name, int num, City departureCity, City arrivalCity,
-        //                  LocalDateTime dateTime, Duration duration, double price, List<Seat> seats)
+    /**
+     * This method creates or uses random data to create flight objects and store them in the flights list.
+     * @param flightCount number of flights to generate
+     */
+    private void generateFlights(int flightCount) {
+        /*
+        Data that needs to be generated:
 
-        final int flightCount = 6;
+        public Flight(String name, int num, City departureCity, City arrivalCity,
+                  LocalDateTime dateTime, Duration duration, double price, List<Seat> seats)
+         */
 
+        // Create list of names to randomly pick from
         List<String> names = Arrays.asList("Delta", "American", "JetBlue", "Spirit", "United", "Allegiant");
 
-        LocalDateTime now = LocalDateTime.now();
-        List<LocalDateTime> dateTimes = Arrays.asList(now);
+        // create list of times starting with today's date and every day for the next 3 days
+        LocalDateTime now = LocalDateTime.now(); // get today's time
+        List<LocalDateTime> dateTimes = new ArrayList<>();
 
-        for(int i=1; i < 7; i++) {
+        dateTimes.add(now); // initialize list with today
+
+
+        // start with current date time and loop to addd extra times
+        for(int i=1; i <= 3; i++) {
             dateTimes.add(now.plusDays(i));
         }
 
-        
-        // generate duration
-        // generate price
-        // generate seats
+        // TODO: Create list of possible durations
 
+        // TODO: generate seats
+
+        /* For the requested number of flights, pull a random name from the list, generate a random flight number,
+           pull random departure and arrival cities, date, duration, and seats list
+         */
         for(int i=0; i < flightCount; i++) {
+            /* generate a random number between 0 and the last element in the list, then fetch the name that is in that
+               position of the list
+             */
             int nameIndex = random.nextInt(names.size());
             String flightName = names.get(nameIndex);
 
-            // pick the random data
+            // generate a random flight number
             int flightNum = this.generateFlightNum();
 
+            /* generate a random number between 0 and the last element in the cities list, then grab the city that
+               is in that element
+             */
             int departureCityIndex = random.nextInt(this.cities.size());
             City departureCity = this.cities.get(departureCityIndex);
 
+            // repeat for arrival city
             int arrivalCityIndex = random.nextInt(this.cities.size());
             City arrivalCity = this.cities.get(arrivalCityIndex);
 
+            // if the destination and arrival are the same, pick a different arrival city
             while (arrivalCity.equals(departureCity)) {
                 arrivalCityIndex = random.nextInt(this.cities.size());
                 arrivalCity = this.cities.get(arrivalCityIndex);
             }
 
-            int dateTimeIndex = random.nextInt(dateTimes.size()); // gives random time index
+            // pick a number between 0 and the end of the date times list, then fetch the date time at that index
+            int dateTimeIndex = random.nextInt(dateTimes.size());
             LocalDateTime dateTime = dateTimes.get(dateTimeIndex);
 
-            this.flights.add(new Flight(flightName, flightNum, departureCity, arrivalCity, dateTime, null, 0, null));
+            // generate the cost of the trip
+            double price = this.generateCost();
+
+            // generate a new flight based on that information
+            this.flights.add(new Flight(flightName, flightNum, departureCity, arrivalCity, dateTime, null, price, null));
         }
     }
 
-
-    // cannot complete untill a completed seats class is created.
-//    public Seats[] generateSeats(){
-//        return new Seats[10];
-//    }
-    
+    /**
+     * Generates a random cost for the flight
+     * @return cost
+     */
     public double generateCost(){
         Random rand = new Random();
         double cost = rand.nextDouble() * 1000;
         DecimalFormat df = new DecimalFormat("#.00");
         return  Double.valueOf(df.format(cost));
     }
+
+    /**
+     * Generates a random flight number between 1 and 1000
+     * @return flightNum
+     */
     public int generateFlightNum(){
         Random rands = new Random();
         int flightNum = rands.nextInt(9999) + 1;
         return flightNum;
-    }
-    public int generateFlightTime(){
-        return 0;
-    }
-    public String generateName(int flightNum, String departureCity, String arrivalCity){
-        // Pieces of Flight Name: <Airline> <Flight Number> <Departure City> - <Arrival City>
-        // Ex: United 5701 ATL-CHA
-        
-        // Flow
-        // 1. Generate random Airline
-        // 2. Construct name from airline, flight num, departure city, and arrival city
-        return  null;
     }
 }
 
