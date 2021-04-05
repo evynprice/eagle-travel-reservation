fe0ee2d15afc9180907fecc043134a337f43af9c
evynprice <me@evynprice.com>
2021-04-05 15:23:26 -0400

    Started work on flight generation

--- src/com/goldeneagle/reservation/app/App.java
+++ src/com/goldeneagle/reservation/app/App.java
@@ -1,9 +1,12 @@
 package com.goldeneagle.reservation.app;
 
+import com.goldeneagle.reservation.resources.DataGenerator;
 import com.goldeneagle.reservation.ui.CommandLine;
 
 public class App {
     public static void main(String[] args) {
+        DataGenerator data = new DataGenerator();
+
         CommandLine cl = new CommandLine();
         cl.init();
     }
--- src/com/goldeneagle/reservation/resources/DataGenerator.java
+++ src/com/goldeneagle/reservation/resources/DataGenerator.java
@@ -5,7 +5,9 @@
  */
 package com.goldeneagle.reservation.resources;
 import java.text.DecimalFormat;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 /**
@@ -13,17 +15,68 @@ import java.util.Random;
  * @author Joeseph Jackson
  */
 public class DataGenerator {
-    private List<City> cities;
-    private List<Flight> flightList;
- 
+    private List<City> cities = new ArrayList<>();
+    private List<Flight> flights = new ArrayList<>();
+    private Random random = new Random();
+
     public DataGenerator() {
+
         // generate cities
         this.cities.add(new City("Chattanooga, TN"));
         this.cities.add(new City("Nashville, TN"));
         this.cities.add(new City("Chicago, IL"));
         this.cities.add(new City("Los Angeles, CA"));
         this.cities.add(new City("Atlanta, GA"));
+
+        this.generateFlights();
     }
+
+    private void generateFlights() {
+        // newFlight(String name, int num, City departureCity, City arrivalCity,
+        //                  LocalDateTime dateTime, Duration duration, double price, List<Seat> seats)
+
+        final int flightCount = 6;
+
+        List<String> names = Arrays.asList("Delta", "American", "JetBlue", "Spirit", "United", "Allegiant");
+
+        LocalDateTime now = LocalDateTime.now();
+        List<LocalDateTime> dateTimes = Arrays.asList(now);
+
+        for(int i=1; i < 7; i++) {
+            dateTimes.add(now.plusDays(i));
+        }
+
+        
+        // generate duration
+        // generate price
+        // generate seats
+
+        for(int i=0; i < flightCount; i++) {
+            int nameIndex = random.nextInt(names.size());
+            String flightName = names.get(nameIndex);
+
+            // pick the random data
+            int flightNum = this.generateFlightNum();
+
+            int departureCityIndex = random.nextInt(this.cities.size());
+            City departureCity = this.cities.get(departureCityIndex);
+
+            int arrivalCityIndex = random.nextInt(this.cities.size());
+            City arrivalCity = this.cities.get(arrivalCityIndex);
+
+            while (arrivalCity.equals(departureCity)) {
+                arrivalCityIndex = random.nextInt(this.cities.size());
+                arrivalCity = this.cities.get(arrivalCityIndex);
+            }
+
+            int dateTimeIndex = random.nextInt(dateTimes.size()); // gives random time index
+            LocalDateTime dateTime = dateTimes.get(dateTimeIndex);
+
+            this.flights.add(new Flight(flightName, flightNum, departureCity, arrivalCity, dateTime, null, 0, null));
+        }
+    }
+
+
     // cannot complete untill a completed seats class is created.
 //    public Seats[] generateSeats(){
 //        return new Seats[10];
