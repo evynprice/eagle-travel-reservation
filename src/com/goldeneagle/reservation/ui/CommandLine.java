81ad19217eddef76cab071892e4bfee033e0b094
evynprice <me@evynprice.com>
2021-04-15 16:04:13 -0400

    Fixed things

--- src/com/goldeneagle/reservation/ui/CommandLine.java
+++ src/com/goldeneagle/reservation/ui/CommandLine.java
@@ -5,6 +5,8 @@ import com.goldeneagle.reservation.resources.DataGenerator;
 import com.goldeneagle.reservation.resources.Flight;
 import com.goldeneagle.reservation.resources.Seat;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -51,40 +53,36 @@ public class CommandLine {
     }
 
     private void bookFlight() {
-        /* Pull in list of cities and print with for loop, ex:
-        1) Chattanooga, TN
-        2) Atlanta, GA
-        3) Nashville, TN
-         */
-
         List<City> cities = data.getCities();
-        City departureCity;
-        City arrivalCity;
+        City selectedDepartureCity;
+        City selectedArrivalCity;
 
         do {
+            System.out.println("Please select your departure city:");
             for (int i=0; i < cities.size(); i++) {
                 System.out.println((i+1) + ") " + cities.get(i).getName());
             }
             System.out.println(cities.size() + 1 + ") Return to menu");
-            System.out.print("\n" + "Please select your departure city: ");
-            int dep = this.scanner.nextInt();
+            System.out.print("\nSelection: ");
+            int departureSelection = this.scanner.nextInt();
 
             // return if quit is selected
-            if (dep == cities.size() + 1) {
+            if (departureSelection == cities.size() + 1) {
                 System.out.println("--------------------------------");
                 return;
             };
 
             // fetch city object by index
-            departureCity = cities.get(dep - 1);
+            selectedDepartureCity = cities.get(departureSelection - 1);
 
             System.out.println("--------------------------------");
+            System.out.println("Please select your arrival city: ");
 
             for (int i=0; i < cities.size(); i++) {
                 System.out.println((i+1) + ") " + cities.get(i).getName());
             }
             System.out.println(cities.size() + 1 + ") Return to menu");
-            System.out.print("\n" + "Please select your arrival city: ");
+            System.out.print("\n" + "Selection: ");
 
             int arr = this.scanner.nextInt();
 
@@ -95,86 +93,137 @@ public class CommandLine {
             };
 
             // fetch city object by index
-            arrivalCity = cities.get(arr - 1);
-            if (departureCity.equals(arrivalCity)) {
+            selectedArrivalCity = cities.get(arr - 1);
+            if (selectedDepartureCity.equals(selectedArrivalCity)) {
                 System.out.println("--------------------------------");
                 System.out.println("Departure City cannot be the same as the Arrival City");
             }
             System.out.println("--------------------------------");
-        } while(departureCity.equals(arrivalCity));
+        } while(selectedDepartureCity.equals(selectedArrivalCity));
 
-        //TODO This should pull a list of possible dates instead of asking for a date
-        System.out.print("Please enter your departure date (MM-DD-YY): ");
-        String date = this.scanner.next();
+
+        // searches through all of the flights and creates a list of possible dateTimeOptions
+        List<LocalDateTime> dateTimeOptions = new ArrayList<>();
+        for (LocalDateTime dateTime : this.data.getDateTimes()) {
+            for (Flight flight : this.data.getFlights()) {
+                if (flight.getDateTime().equals(dateTime) && flight.getArrivalCity().equals(selectedArrivalCity)
+                    && flight.getDepartureCity().equals(selectedDepartureCity)) {
+
+                    if (!dateTimeOptions.contains(dateTime)) {
+                        dateTimeOptions.add(dateTime);
+                    }
+                }
+            }
+        }
+
+        // print all of the possible dates
+        System.out.println("Please select your departure date: ");
+        for (int i=0; i < dateTimeOptions.size(); i++) {
+            LocalDateTime dateTime = dateTimeOptions.get(i);
+            String dateTimeFormatted = dateTime.format(DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+
+            System.out.println((i+1) + ") " + dateTimeFormatted);
+        }
+
+        System.out.println(dateTimeOptions.size() + 1 + ") Return to menu");
+        System.out.print("Selection: ");
+        int selectedDate = this.scanner.nextInt();
+
+        // return if quit is selected
+        if (selectedDate == dateTimeOptions.size() + 1) {
+            System.out.println("--------------------------------");
+            return;
+        };
+
+        LocalDateTime selectedDateTime = dateTimeOptions.get(selectedDate - 1);
         System.out.println("--------------------------------");
 
         // fetch flights that match destination, departure, and date
-        //TODO check every flight in the list, and see if the destination and arrival cities match, then check if the date matches
         List<Flight> selectedFlights = new ArrayList<>();
 
         for (Flight flight : data.getFlights()) {
             // check if selected departure city equals flight departure city
             // check if selected arrival city equals flight arrival city
             // check if selected date equals flight date
-            if (true) {
+            if (flight.getDepartureCity().equals(selectedDepartureCity) &&
+                flight.getArrivalCity().equals(selectedArrivalCity) &&
+                flight.getDateTime().equals(selectedDateTime)) {
                 selectedFlights.add(flight);
             }
         }
 
-        for (int i=0; i < selectedFlights.size(); i++) {
-            Flight flight = selectedFlights.get(i);
-            int hours = (int) flight.getDuration().toHours();
-            int minutes = (int) flight.getDuration().minusHours(hours).toMinutes();
-
-            //TODO set this to print actual data (flight name, num, depCity, arrCity, dateTime, duration, price, and how many seats out of 25 are available
-            System.out.printf((i+1) + ") %s %s-%s: %dh %dm - $%5.2f %n", flight.getName(), "CHA", "ATL", hours, minutes, flight.getPrice());
-        }
-
-        System.out.println(selectedFlights.size() + 1 + ") Return to menu");
-        System.out.print("\n" + "Please select a flight: ");
-
-        int fli = this.scanner.nextInt();
-
-        // return if quit is selected
-        if (fli == selectedFlights.size() + 1) return;
-
-        // fetch flight object by index
-        Flight flight = selectedFlights.get(fli - 1);
-
+        boolean seatIsNotAvailable = true;
         Seat yourSeat = null;
-        //TODO check if seats are available, if not then return to prompt. If so then continue
-        for (Seat seat : flight.getSeats()) {
-            if (seat.isAvailable()) {
-                yourSeat = seat;
+
+        System.out.println("Please select a flight: ");
+        do {
+            for (int i=0; i < selectedFlights.size(); i++) {
+                Flight flight = selectedFlights.get(i);
+                int hours = (int) flight.getDuration().toHours();
+                int minutes = (int) flight.getDuration().minusHours(hours).toMinutes();
+
+                int availableSeatCount = 0;
+
+                for (Seat seat : flight.getSeats()) {
+                    if (seat.isAvailable()) {
+                        availableSeatCount++;
+                    }
+                }
+
+                //TODO set this to print actual data (flight name, num, depCity, arrCity, dateTime, duration, price, and how many seats out of 25 are available
+                System.out.printf((i+1) + ") %s %s-%s: %dh %dm - $%5.2f %d/25 seats %n", flight.getName(),
+                        flight.getDepartureCity().getAirport(), flight.getArrivalCity().getAirport(),
+                        hours, minutes, flight.getPrice(), availableSeatCount);
             }
-        }
 
-        if (yourSeat == null) {
-            // print sorry there were no seats, repeat the selection
-        }
+            System.out.println(selectedFlights.size() + 1 + ") Return to menu");
+            System.out.print("Selection: ");
 
-        System.out.println("--------------------------------");
+            int fli = this.scanner.nextInt();
 
-        System.out.print("Please enter your email: ");
-        String email = this.scanner.next();
+            // return if quit is selected
+            if (fli == selectedFlights.size() + 1) return;
 
-        //possibly a way to deal with double booking but still needs an option to stop.
-        while(email.equals(seat.getEmail))
-        {
+            // fetch flight object by index
+            Flight flight = selectedFlights.get(fli - 1);
 
-            System.out.println("Email is already booked to a flight");
+            //TODO check if seats are available, if not then return to prompt. If so then continue
+            for (Seat seat : flight.getSeats()) {
+                if (seat.isAvailable()) {
+                    yourSeat = seat;
+                    seatIsNotAvailable = false;
+                }
+            }
 
+            if (seatIsNotAvailable) {
+                System.out.println("--------------------------------");
+                System.out.println("There were no seats available for the selected flight. Please select a different flight");
+            }
+            System.out.println("--------------------------------");
+
+        } while (seatIsNotAvailable);
+
+        boolean doesEmailExist;
+        String email;
+
+        do {
+            doesEmailExist = false;
             System.out.print("Please enter your email: ");
             email = this.scanner.next();
-        }
 
+            for (Flight listFlight : this.data.getFlights()) {
+                for (Seat listSeat : listFlight.getSeats()) {
+                    if (listSeat.getEmail().equals(email)) {
+                        doesEmailExist = true;
+                    }
+                }
+            }
 
-
-        System.out.println("--------------------------------");
-
-
-
-        //TODO check if this email is currently in use in a flight, if so ask for a different email
+            if (doesEmailExist) {
+                System.out.println("That email is already being used, please enter a new email");
+            }
+            System.out.println("--------------------------------");
+        } while (doesEmailExist);
 
         yourSeat.setAvailable(false);
         yourSeat.setEmail(email);
